f6d6d06f4492c2860ce7657a209b8325b59f66b8
astghik <astghikg512@gmail.com>
2024-02-24 02:09:57 +0400

    arr streing

--- task2/Arr_string/main.cpp
+++ task2/Arr_string/main.cpp
@@ -10,59 +10,35 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <cstring>
+#include <iostream>
+#include <QtCore/qcoreapplication.h>
 
 
 
-int max(int*, const int);
-int min(int*, const int);
+int max(int*, const size_t);
+int min(int*, const size_t);
 int str_len(const char*);
 char* com_cat(char*, const char* );
-int avr(int*, const int);
+int avr(int*, const size_t);
 void displayArray(int*, int);
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+       QApplication a(argc, argv);
 
-
-
-    QWidget mainWindow;
+       QWidget mainWindow;
        QVBoxLayout layout(&mainWindow);
 
-       QLineEdit inputLineEdit(&mainWindow);
-       layout.addWidget(&inputLineEdit);
-
-       QPushButton submitButton("Submit", &mainWindow);
-       layout.addWidget(&submitButton);
-
-       QLabel resultLabel(&mainWindow);
-       layout.addWidget(&resultLabel);
-
-       char* str = new char[256];
-       char* str1 = new char[256];
 
 
-     QObject::connect(&submitButton, &QPushButton::clicked, [&]() {
-         QString userInput = inputLineEdit.text();
-
-
-                strcpy(str, userInput.toLocal8Bit().constData());
-                strcpy(str1, userInput.toLocal8Bit().constData());
-
-
-                resultLabel.setText("Строка 1: " + QString(str) + "\nСтрока 2: " + QString(str1));
-
-
-                inputLineEdit.clear();
-     });
 
      mainWindow.show();
 
-   bool ok;
-   int size = QInputDialog::getInt(nullptr, "Input", "Enter the size of the array:", 0, 0, INT_MAX, 1, &ok);
+   bool ok = true;
+   size_t size = QInputDialog::getInt(nullptr, "Input", "Enter the size of the array:", 0, 0, INT_MAX, 1, &ok);
 
    if (!ok) {
-          // The user pressed Cancel, handle accordingly
+
           return 0;
       }
 
@@ -70,7 +46,7 @@ int main(int argc, char *argv[])
 
     QString inputText = QInputDialog::getText(nullptr, "Input", "Enter space-separated elements:");
 
-      // Parse the input and fill the array
+
       QStringList inputList = inputText.split(' ');
       for (int i = 0; i < size; ++i) {
           arr[i] = inputList.value(i).toInt();
@@ -87,13 +63,38 @@ int main(int argc, char *argv[])
                 displayArray(arr, size);
                 QLabel label(labelText);
                 label.show();
-                delete [] arr;
-                delete [] str;
-                delete [] str1;
+
+                char* strC = qstrdup(QInputDialog::getText(nullptr, "Input", "Enter the first string:", QLineEdit::Normal, "", &ok).toUtf8().constData());
+
+                    if (!ok) {
+
+                        delete[] arr;
+                        delete[] strC;
+                        return 0;
+                    }
+
+                    char* strC1 = qstrdup(QInputDialog::getText(nullptr, "Input", "Enter the first string:", QLineEdit::Normal, "", &ok).toUtf8().constData());
+
+                      if (!ok) {
+
+                          delete[] arr;
+                          delete[] strC;
+                          delete[] strC1;
+                          return 0;
+                      }
+
+                      qDebug() << "You entered:";
+                      qDebug() << "First string:" << strC;
+                      qDebug() << "Second string:" << strC1;
+                      strC = com_cat(strC,strC1);
+                      qDebug() << "First string new:" << strC;
+                      delete [] arr;
+                      delete [] strC;
+                      delete [] strC1;
     return a.exec();
 }
 
-int max(int* arr, const int s){
+int max(int* arr, const size_t s){
     int m = INT_MIN;
     for(int i = 0; i < s; ++i ){
         if(arr[i] > m){
@@ -103,7 +104,7 @@ int max(int* arr, const int s){
     return m;
 }
 
-int min(int* arr, const int s){
+int min(int* arr, const size_t s){
     int m = INT_MAX;
     for(int i = 0; i < s; ++i ){
         if(arr[i] < m){
@@ -113,7 +114,7 @@ int min(int* arr, const int s){
     return m;
 }
 
-int avr(int* arr, const int s){
+int avr(int* arr, const size_t s){
     int sum = 0;
     for(int i = 0; i < s; ++i){
         sum += arr[i];
@@ -130,19 +131,34 @@ int str_len(const char* str){
 }
 
 
-char* com_cat(char*str, const char* str1){
+char* com_cat(char* str, const char* str1) {
+    int l = str_len(str);
+    int l1 = str_len(str1);
+    char* tmp = new char[l + l1 + 1];
     int i = 0;
-    int j = 0;
-    while(str1[j] != '\0'){
-        while(str[i] != '\0'){
-            ++i;
-        }
-        str[i] = str1[j];
-        ++i;
-        ++j;
+
+
+    for (i = 0; i < l; ++i) {
+        tmp[i] = str[i];
     }
 
+
+    for (int j = 0; j < l1; ++j, ++i) {
+        tmp[i] = str1[j];
+    }
+
+
+    tmp[i] = '\0';
+
+
+    delete[] str;
+    str = tmp;
+
+    return str;
 }
+
+
+
 
 
 void displayArray(int* arr, int size) {
