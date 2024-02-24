ee5411483c80e36672b3ae5b0125e542e4c57c58
Astg582 <astghikg515@gmail.com>
2024-02-24 17:54:57 +0400

    todo

--- task2/to_do/main.cpp
+++ task2/to_do/main.cpp
@@ -8,17 +8,17 @@
 class ToDoList : public QWidget {
 public:
     ToDoList(QWidget *parent = nullptr) : QWidget(parent) {
-        // Sozdanie osnovnykh elementov interfeysa
+
         taskInput = new QLineEdit(this);
         addButton = new QPushButton("Add", this);
         deleteButton = new QPushButton("Delete", this);
         taskList = new QListWidget(this);
 
-        // Podkljuchenie funktsiy k knopkam
+
         connect(addButton, &QPushButton::clicked, this, &ToDoList::addTask);
         connect(deleteButton, &QPushButton::clicked, this, &ToDoList::deleteTask);
 
-        // Raspredelenie elementov v okne
+
         QVBoxLayout *layout = new QVBoxLayout(this);
         layout->addWidget(taskInput);
         layout->addWidget(addButton);
@@ -31,7 +31,7 @@ public:
 
 private slots:
     void addTask() {
-        // Dobavlenie zadachi v spisok
+
         QString task = taskInput->text();
         if (!task.isEmpty()) {
             taskList->addItem(task);
@@ -40,7 +40,7 @@ private slots:
     }
 
     void deleteTask() {
-        // Udalenie vybrannoj zadachi
+
         int selectedRow = taskList->currentRow();
         if (selectedRow != -1) {
             delete taskList->takeItem(selectedRow);
