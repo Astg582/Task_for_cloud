92cf8e9f5f7591450b9f91d15d831f94db03cf1d
Astg582 <astghikg515@gmail.com>
2024-03-01 14:07:10 +0400

    virt

--- task5/hiar/main.cpp
+++ task5/hiar/main.cpp
@@ -5,6 +5,9 @@
 class Shape {
 public:
     virtual double area() const = 0;
+    void exept() const {
+        throw std::invalid_argument("invalid argument");
+    }
     virtual ~Shape() = default;
 };
 
@@ -15,12 +18,12 @@ private:
 public:
     inline Circul(double r) : radius(r) {
         if (radius <= 0) {
-            throw std::invalid_argument("Radius must be positive");
+            exept();
         }
     }
 
 
-    void set_radius(double r){if(r > 0){this->radius = r;}else {throw std::invalid_argument("Length and width must be positive");}}
+    void set_radius(double r){if(r > 0){this->radius = r;}else {exept();}}
     double get_radius() const{return radius;}
 
     double area() const override{return 3.14 * radius * radius;}
@@ -36,17 +39,17 @@ public:
 
 inline Rectangle(double l, double w) : length(l), width(w) {
     if (length < 0 || width < 0) {
-        throw std::invalid_argument("Length and width must be positive");
+        exept();
     }
 }
 
-void set_length(double l) {if(l > 0 ) {this->length = l;} else {throw std::invalid_argument("Length and width must be positive");} }
+void set_length(double l) {if(l > 0 ) {this->length = l;} else {exept();}}
 
 inline double get_length() const {
     return length;
 }
 
-void set_width(double w) {if(w > 0) { this->width = w; } else {throw std::invalid_argument("Length and width must be positive");}}
+void set_width(double w) {if(w > 0) { this->width = w; } else {exept();}}
 
 inline double get_width() const {
     return width;
@@ -56,6 +59,10 @@ inline double area() const {
     return length * width;
 }
 };
+
+void Area (Shape* ptr){
+    std::cout << ptr->area()<< std::endl;
+}
 
 int main(int argc, char *argv[])
 {
@@ -67,6 +74,11 @@ int main(int argc, char *argv[])
     re.set_length(4);
     std::cout << circ.get_radius() << std::endl;
     std::cout << re.get_length() << "  " << re.get_width() << std::endl;
+    Circul* ptr = &circ;
+    Rectangle* pR = &re;
+    Area(ptr);
+    Area(pR);
+
     w.show();
     return a.exec();
 }
